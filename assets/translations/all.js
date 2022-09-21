21a93c7c9f97f73f82645540a2307f88115ac08a
Mercea Emil <office@insightfeatures.com>
2022-09-21 20:28:17 +0300

    [ch31387] Missing Russian translations part I (list)

--- assets/translations/all.js
+++ assets/translations/all.js
@@ -20,7 +20,51 @@ const translations = {
     EMAIL: 'Email',
     OK_BTN_TEXT: 'Enregistrer',
     CANCEL_BTN_TEXT: 'Fermer'
-  }
+  },
+  ar: {
+    PROFILE: 'الملف الشخصي',
+    MY_PROFILE: 'ملفي',
+    SIGN_OUT: 'خروج',
+    NAME: 'اسم',
+    AVAILABLE_WORKSPACES: 'مساحات العمل المتاحة',
+    MY_GROUPS: 'مجموعاتي',
+    EMAIL: 'البريد الإلكتروني',
+    OK_BTN_TEXT: 'يحفظ',
+    CANCEL_BTN_TEXT: 'قريب'
+  },
+  es: {
+    PROFILE: 'Perfil',
+    MY_PROFILE: 'Mi perfil',
+    SIGN_OUT: 'Desconectar',
+    NAME: 'Nombre',
+    AVAILABLE_WORKSPACES: 'Espacios de trabajo disponibles',
+    MY_GROUPS: 'Mis grupos',
+    EMAIL: 'Correo electrónico',
+    OK_BTN_TEXT: 'Salvo',
+    CANCEL_BTN_TEXT: 'Cerca'
+  },
+  pt: {
+    PROFILE: 'Perfil',
+    MY_PROFILE: 'Meu perfil',
+    SIGN_OUT: 'Sair',
+    NAME: 'Nome',
+    AVAILABLE_WORKSPACES: 'Áreas de trabalho disponíveis',
+    MY_GROUPS: 'Meus grupos',
+    EMAIL: 'E-mail',
+    OK_BTN_TEXT: 'Salvar',
+    CANCEL_BTN_TEXT: 'Perto'
+  },
+  ru: {
+    PROFILE: 'Профиль',
+    MY_PROFILE: 'Мой профиль',
+    SIGN_OUT: 'Выход',
+    NAME: 'Имя',
+    AVAILABLE_WORKSPACES: 'Доступные рабочие области',
+    MY_GROUPS: 'Мои группы',
+    EMAIL: 'Эл. адрес',
+    OK_BTN_TEXT: 'Сохранять',
+    CANCEL_BTN_TEXT: 'закрывать'
+  },
 };
 
 export default translations;
